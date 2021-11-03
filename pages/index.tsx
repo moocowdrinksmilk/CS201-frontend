df89743e71d0fe3656d7f3820b976e243b4054b3
Rui Xian <tayrx9988@gmail.com>
2021-11-03 23:18:37 +0800

    add react icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,23 @@ import { useState } from 'react'
 // @ts-ignore
 import GoogleMapReact from 'google-map-react';
 
+const AnyReactComponent = ({ text }: any) => <div className="bg-indigo-500 text-5xl">{text}</div>;
+
 const Home: NextPage = () => {
 
     const [sort, setSort] = useState(0)
     const [data, setData] = useState([])
 
     const typeOfSorts = ['quick-sort', 'merge-sort', 'heap-sort']
+
+    const defaultProps = {
+        center: {
+          lat: 10.99835602,
+          lng: 77.01502627
+        },
+        zoom: 11
+      };
+    
 
     const sortDestinations = async () => {
         const res = await axios.get(`http://localhost:8080/${typeOfSorts[sort]}/merge-sort?originLat=40.0175444&originLong=-105.2833481&maxDist=500`)
@@ -20,9 +31,6 @@ const Home: NextPage = () => {
             <h1 className="flex flex-row justify-center text-8xl w-full text-center">
                 Bar Crawl 1.0
             </h1>
-            <h2>
-                Efficient crawler
-            </h2>
 
             <div className="w-full flex flex-row justify-center gap-10 text-xl">
                 <button className="bg-yellow-300 hover:bg-yellow-200 hover:shadow-md px-4 py-2 rounded-md">
@@ -36,10 +44,22 @@ const Home: NextPage = () => {
                 <button className="bg-yellow-300 hover:bg-yellow-200 hover:shadow-md px-4 py-2 rounded-md">
                     Heap Sort
                 </button>
-            </div> 
+            </div>
 
-            <div className="flex flex-col items-center gap-10">
-
+            <div className="flex flex-col items-center gap-10 py-10">
+                <div className="w-96 h-96">
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: "" }}
+                        defaultCenter={defaultProps.center}
+                        defaultZoom={defaultProps.zoom}
+                    >
+                        <AnyReactComponent
+                            lat={10.99835602}
+                            lng={77.01502627}
+                            text="My Marker"
+                        />
+                    </GoogleMapReact>
+                </div>
             </div>
         </div>
     )
