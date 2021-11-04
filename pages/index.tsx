4746ea81b61788b81e0c98923ef6f94cd87aaa02
Rui Xian <tayrx9988@gmail.com>
2021-11-04 13:49:03 +0800

    address and name of restaurant

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 // @ts-ignore
 import GoogleMapReact from 'google-map-react';
 import { FaMapMarkerAlt } from 'react-icons/fa'
-import { Spin } from 'antd';
+import { Spin, Timeline } from 'antd';
 
 const AnyReactComponent = ({ text }: any) => <FaMapMarkerAlt size={30} color="red" />;
 
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
 
     const [sort, setSort] = useState(1)
     const [data, setData] = useState([])
+    const [routeData, setRouteData] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [lat, setLat] = useState(40.0175444)
     const [lng, setLng] = useState(-105.2833481)
@@ -41,8 +42,14 @@ const Home: NextPage = () => {
     //     })
     // }
 
+    const getRoute = async (plat, plng ) => {
+        const { data } = await axios.get(`http://localhost:8080/filter-sort/budget/route?originLat=${plat}&originLong=${plng}&maxDist=500&budget=100`)
+        setRouteData(data)
+    }
+
     const sortDestinations = async () => {
         setIsLoading(true)
+        getRoute(lat, lng)
         const { data } = await axios.get(`http://localhost:8080/filter-sort/${typeOfSorts[sort]}?originLat=${lat}&originLong=${lng}&maxDist=500`)
         setData(data.destinations)
         setIsLoading(false)
@@ -53,7 +60,8 @@ const Home: NextPage = () => {
         setIsLoading(true)
         setLat(obj.lat)
         setLng(obj.lng)
-        const { data } = await axios.get(`http://localhost:8080/filter-sort/${typeOfSorts[sort]}?originLat=${lat}&originLong=${lng}&maxDist=500`)
+        const { data } = await axios.get(`http://localhost:8080/filter-sort/${typeOfSorts[sort]}?originLat=${obj.lat}&originLong=${obj.lng}&maxDist=500`)
+        getRoute(obj.lat, obj.lng)
         setData(data.destinations)
         setIsLoading(false)
     }
@@ -107,6 +115,26 @@ const Home: NextPage = () => {
                         }
                     </GoogleMapReact>
                 </div>
+
+                <Timeline
+                mode="alternate">
+                        {
+                            routeData && routeData.map((item, index) => {
+                                return (
+                                    <Timeline.Item>
+                                        <div className="flex flex-col items-start gap-1 w-64">
+                                            <span>
+                                                {item.name}
+                                            </span>
+                                            <span className="text-gray-500">
+                                                {item.address}
+                                            </span>
+                                        </div>
+                                    </Timeline.Item>
+                                )
+                            })
+                        }
+                </Timeline>
             </div>
         </div>
     )
